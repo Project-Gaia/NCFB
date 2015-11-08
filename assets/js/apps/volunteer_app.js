a312f3a149de66d622744d6aa7aab49ee886c003
MarkSun <sunmark14@gmail.com>
2015-11-08 11:12:25 -0500

    Fixed back button with routing

--- assets/js/apps/volunteer_app.js
+++ assets/js/apps/volunteer_app.js
@@ -2,15 +2,25 @@ VolunteerManager.module("VolunteerApp", function(VolunteerApp, VolunteerManager,
    Backbone, Marionette, $, _) {
      VolunteerApp.Router = Marionette.AppRouter.extend({
        appRoutes: {
+         "welcomeScreen": "displayWelcomeScreen",
          "applicableOptions": "displayApplicableOptions"
        }
      })
 
      var API = {
        displayApplicableOptions: function(){
-         console.log("route to applicable options was triggered");
+         VolunteerApp.ApplicableOptions.Controller.display();
          //VolunteerApp.ApplicableOptions.Controller.display();
+       },
+       displayWelcomeScreen: function(){
+         VolunteerApp.WelcomeScreen.Controller.display();
        }
-     }
+     };
 
-   }
+     VolunteerApp.on("start", function(){
+       new VolunteerApp.Router({
+         controller: API
+       })
+     })
+
+   });
