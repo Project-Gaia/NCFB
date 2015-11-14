342425c6f924c96c3f685625326d65382ebd7ab6
Mark Sun <sunmark14@gmail.com>
2015-11-14 10:21:10 -0500

    Fixed an issue with Modal Dialog

--- assets/js/apps/sign_up_view.js
+++ assets/js/apps/sign_up_view.js
@@ -1,5 +1,12 @@
 VolunteerManager.module("VolunteerApp.SignUp", function(SignUp
   , VolunteerManager, Backbone, Marionette, $, _){
+
+    // Define our modal view class for the terms
+    var Modal = Backbone.Modal.extend({
+      template: '#modal-template',
+      cancelEl: '.bbm-button'
+    });
+
     SignUp.Page = Marionette.ItemView.extend({
       template: "#sign-up-template",
 
@@ -11,16 +18,24 @@ VolunteerManager.module("VolunteerApp.SignUp", function(SignUp
         txtboxEmergencyContactNumber: "#sign-up-emergency-contact-phone",
         checkboxAgreeTerms: "#sign-up-terms-agreement",
         btnCreate: "#sign-up-create-btn",
-        btnCancel: "#sign-up-cancel-btn"
+        btnCancel: "#sign-up-cancel-btn",
+        linkTerms: "#terms-link"
       },
 
       events: {
         "click @ui.btnCreate": "BtnCreateOnClick",
-        "click @ui.btnCancel": "BtnCancelOnClick"
+        "click @ui.btnCancel": "BtnCancelOnClick",
+        "click @ui.linkTerms": "LinkTermsOnClick",
       },
 
       onRender: function(){
         this.ui.datepicker.datepicker();
+      },
+
+      LinkTermsOnClick: function(){
+        // Render an instance of your modal
+        var modalView = new Modal();
+        $('#terms-modal-container').html(modalView.render().el);
       },
 
       BtnCreateOnClick: function(){
